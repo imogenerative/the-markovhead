e7d79e46028f17f7ddfce8c627a47bf83f4986d2
Imogen Fulton <imogen@dfdt.me>
2018-03-01 22:15:53 +1100

    what value of n did you use?? can you bump it up one more??

--- rand.py
+++ rand.py
@@ -4,6 +4,6 @@ import markov_novel
 with open('corpus.txt') as f:
     text = f.read()
 # Build the model.
-text_model = markovify.Text(text)
+text_model = markovify.Text(text, state_size=3)
 novel = markov_novel.Novel(text_model, chapter_count=1)
 novel.write(novel_title='markov-shrugged', filetype='md')
